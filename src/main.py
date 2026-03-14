eb45b0cb16c236fed99207a54062bceef3ea63a4
thefooliest <1xsdavidsx5@gmail.com>
2026-03-14 10:32:51 -0500

    added security headers for responses

--- src/main.py
+++ src/main.py
@@ -25,6 +25,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.middleware("http")
+async def add_security_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
+    return response
+
 # Routers
 app.include_router(auth_controller.router)
 app.include_router(project_controller.router)
